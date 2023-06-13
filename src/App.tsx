eb065ed6d0d2422b59d2337a9dc3366e69064dde
corlard3y <corlardey@gmail.com>
2023-06-13 11:41:07 +0100

    set up web3

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as PushAPI from "@pushprotocol/restapi";
 
 function App() {
+   const [walletAddress, setWalletAddress] = useState<string>(
+    "0xD8634C39BBFd4033c0d3289C4515275102423681"
+  );
+
+  useEffect(() => {
+    console.log("walletAddress:", walletAddress);
+    async function fetchNotifications() {
+      const res = await PushAPI.user.getFeeds({
+        user: `eip155:5:${walletAddress}`, // user address in CAIP
+        env: "staging" as any,
+      });
+      console.log(res);
+    }
+
+    fetchNotifications();
+  }, [walletAddress]);
+  
   return (
     <div className="App">
       <header className="App-header">
